d1ca34bb24fd86d0a1edc40381e417b88ec6dd0b
Subachev Denis <51040974+Snowdenru@users.noreply.github.com>
2022-05-08 13:05:09 +0300

    Финальный экран

--- level/final_win.py
+++ level/final_win.py
@@ -1,60 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (
-        QApplication, QWidget, 
-        QHBoxLayout, QVBoxLayout, 
-        QGroupBox, QRadioButton,
-        QPushButton, QLabel, QListWidget, QLineEdit)
-
+       QApplication, QWidget,
+       QHBoxLayout, QVBoxLayout, QGridLayout,
+       QGroupBox, QRadioButton,
+       QPushButton, QLabel, QListWidget, QLineEdit)
+      
 from instr import *
-from second_win import *
-
-       
-class MainWin(QWidget):
-    def __init__(self):
-        ''' окно, в котором располагается приветствие '''
-        super().__init__()
-
-        # создаём и настраиваем графические эелементы:
-        self.initUI()
-
-        #устанавливает связи между элементами
-        self.connects()
-
-        #устанавливает, как будет выглядеть окно (надпись, размер, место)
-        self.set_appear()
-
-        # старт:
-        self.show()
-
-    def initUI(self):
-        ''' создает графические элементы '''
-        self.btn_next = QPushButton(txt_next, self)
-        self.hello_text = QLabel(txt_hello)
-        self.instruction = QLabel(txt_instruction)
-
-        self.layout_line = QVBoxLayout()
-        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
-        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft) 
-        self.layout_line.addWidget(self.btn_next, alignment = Qt.AlignCenter)          
-        self.setLayout(self.layout_line)
-
-    
-    def next_click(self):
-        self.tw = TestWin()
-        self.hide()
-
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)
-
-    ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width, win_height)
-        self.move(win_x, win_y)
-
-app = QApplication([])
-mw = MainWin()
-app.exec_()
+ 
+class FinalWin(QWidget):
+   def __init__(self):
+       ''' окно, в котором проводится опрос '''
+       super().__init__()
+ 
+       # создаём и настраиваем графические элементы:
+       self.initUI()
+ 
+       #устанавливает, как будет выглядеть окно (надпись, размер, место)
+       self.set_appear()
+      
+       # старт:
+       self.show()
+ 
+   def initUI(self):
+       ''' создаёт графические элементы '''
+       self.workh_text = QLabel(txt_workheart)
+       self.index_text = QLabel(txt_index)
+ 
+       self.layout_line = QVBoxLayout()
+       self.layout_line.addWidget(self.index_text, alignment = Qt.AlignCenter)
+       self.layout_line.addWidget(self.workh_text, alignment = Qt.AlignCenter)        
+       self.setLayout(self.layout_line)
+ 
+   ''' устанавливает, как будет выглядеть окно (надпись, размер, место) '''
+   def set_appear(self):
+       self.setWindowTitle(txt_finalwin)
+       self.resize(win_width, win_height)
+       self.move(win_x, win_y)
